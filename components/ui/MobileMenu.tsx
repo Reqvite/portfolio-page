da47a3223dff3329510c556329ac0eebd06ce94f
Reqvite <reqvite2233@gmail.com>
2023-04-26 13:30:21 +0300

    Update MobileMenu.tsx

--- components/ui/MobileMenu.tsx
+++ components/ui/MobileMenu.tsx
@@ -1,6 +1,12 @@
 import { FC } from "react";
 import { HamburgerIcon } from "@chakra-ui/icons";
-import { IconButton, Menu, MenuButton, MenuList } from "@chakra-ui/react";
+import {
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import NavbarItem from "../Header/NavbarItem";
 import { navigation } from "@/data/navigation";
 
@@ -15,7 +21,12 @@ const MobileMenu: FC = () => {
         bg="accentColor.100"
         ml={2}
       />
-      <MenuList>
+      <MenuList
+        bg={useColorModeValue(
+          "rgb(244, 248, 247, 0.8)",
+          "rgba(32, 32, 35, 0.8)"
+        )}
+      >
         {navigation.map(({ id, title, path }) => (
           <NavbarItem key={id} title={title} path={path} />
         ))}
